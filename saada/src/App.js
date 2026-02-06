a3b7c1499198a064ce449ad0a2bf7ac3637d1a75
Kihoro Wangui <94446783+KihoroWangui@users.noreply.github.com>
2026-02-06 11:44:27 +0300

    COMMIT

--- saada/src/App.js
+++ saada/src/App.js
@@ -1,5 +1,5 @@
 ﻿import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./Context/AuthContext";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
